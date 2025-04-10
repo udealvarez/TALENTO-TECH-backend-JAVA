c1f44c5a2cbd70a5903fee23f9a250921770892d
Eduardo AlvareZ <udealvarez@icloud.com>
2025-04-10 19:51:03 -0300

    Ejercicio practico | Clase 1

---
+++ Clase-1/Ejercicio-Practico-1/src/main/java/com/techlab/inicio/SaludoPersonalizado.java
@@ -0,0 +1,9 @@
+package com.techlab.inicio;
+
+public class SaludoPersonalizado {
+
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO");
+        System.out.println("BIENVENIDO A TALENTO TECH");
+    }
+}

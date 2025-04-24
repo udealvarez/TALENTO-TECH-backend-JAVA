178c64b537e6f0cb33b63a0952b2272814f21ac2
Eduardo AlvareZ <udealvarez@icloud.com>
2025-04-24 01:17:19 -0300

    Estructura de ArrayList

---
+++ Clase-3/Material-Apliatorio/src/main/java/Cuatro_Listas.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Cuatro_Listas {
+    public static void main(String[] args) {
+
+        ArrayList<String> listaProductos = new ArrayList<>();
+        listaProductos.add("Televisor");
+        listaProductos.add("Heladera");
+        listaProductos.add("Mesa");
+        listaProductos.add("Sillas");
+        listaProductos.add("Lavarropas");
+
+        System.out.println(listaProductos);     // en la consola se muesstra como un array
+    }
+}

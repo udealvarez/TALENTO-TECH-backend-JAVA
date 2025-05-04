5a085d42f1f02525b232ecf38061bf827a8f8438
Eduardo Alvarez <udealvarez@icloud.com>
2025-05-04 17:02:39 -0300

    Material ampliatorio - Clase 5

--- Clase-5/Material-Ampliatorio/src/main/java/Cliente.java
+++ Clase-5/Material-Ampliatorio/src/main/java/Cliente.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Cliente {
+    String nombre;
+    String email;
+
+
+    public Cliente(String nombre, String email) {
+        this.nombre = nombre;
+        this.email = email;
+    }
 }
--- Clase-5/Material-Ampliatorio/src/main/java/Producto.java
+++ Clase-5/Material-Ampliatorio/src/main/java/Producto.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Producto {
+    String nombre;
+    double precio;
+    int cantidadEnStock;
+
+
+    public void descontarStock(int cantidad){       // método de instancia - es propio de cada producto
+        this.cantidadEnStock -= cantidad;
+    }
+
+    public static double calcularImpuesto(double precio){   // método estático - comun a todos
+        return precio * 0.21;  // 21% de impuestos en ARG
+    }
+
 }
